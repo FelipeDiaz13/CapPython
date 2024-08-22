30de0595e091e2ac30435b0bad5ee25a0dd7a613
FelipeDiaz13 <felipedejesus132002@gmail.com>
2024-08-22 11:50:43 -0600

    Agregacion de color

--- Excepciones.py
+++ Excepciones.py
@@ -1,1 +1,17 @@
-print("Hola mundo")
+# Un ejemplo de error
+#print(1/0) 0-9
+
+numero_var = "hola"
+
+try:
+
+    print(int(numero_var))
+except Exception as error : 
+    print(f" El valor que se recibe no es entero {error}")
+
+print("Hola Mundo")
+
+def ejemplo():
+    return
+
+ejemplo()
---
+++ POO.py
@@ -0,0 +1,16 @@
+class Automovil:
+    def tipo_color(sefl, inColor,inRed,inBlue,inGreen):
+        print(f"El Color Del Auto Es: {inColor}")
+        print(f"El Color Del Auto Es: {inRed}")
+        print(f"El Color Del Auto Es: {inBlue}")
+        print(f"El Color Del Auto Es: {inGreen}")
+
+
+
+Spark = Automovil()
+Aveo = Automovil()
+Bmw = Automovil()
+
+Spark.tipo_color(inColor="Azul",inBlue="255",inGreen="0",inRed="0")
+Aveo.tipo_color(inColor="Azul",inBlue="255",inGreen="0",inRed="0")
+Bmw.tipo_color(inColor="Azul",inBlue="255",inGreen="0",inRed="0")
